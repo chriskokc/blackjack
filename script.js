49b9f715e1d0c8ea1569a963f501b1a041a680ae
Chris Ko <ko_ka_chun@hotmail.com>
2022-07-12 00:11:02 +0100

    fixed a typo in deckOfCard and created a startGame function.

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
 
-const deckOfCard = ["A", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+const deckOfCard = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
 let dealer = [];
 let player = [];
 
+// DOM
+const startBtn = document.querySelector(".bet__btn");
+
+
+// Functions
 const getRandomCard = (cards) => {
     const randomNumber = Math.floor(Math.random() * cards.length);
     const cardDrawn = cards[randomNumber];
     return cardDrawn;
 };
+
+const startGame = () => {
+    for (let i = 0; i < 2; i++) {
+        dealer.push(getRandomCard(deckOfCard));
+        player.push(getRandomCard(deckOfCard));
+    }
+};
+
+// Event listener
+startBtn.addEventListener("click", startGame);
+
 
 
 
