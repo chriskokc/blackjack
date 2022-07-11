ef4e19c5ab54de15d9270032315af93e1dbf365a
Chris Ko <ko_ka_chun@hotmail.com>
2022-07-12 00:36:28 +0100

    changed the positioning of the card cover and added functionality to display cards.

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 
 const deckOfCard = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+const suits = ["♠️", "❤️", "♣️", "♦️"];
 let dealer = [];
 let player = [];
 
 // DOM
 const startBtn = document.querySelector(".bet__btn");
-
+let dealerSide = document.querySelector(".game__dealer");
+let playerSide = document.querySelector(".game__player");
 
 // Functions
 const getRandomCard = (cards) => {
@@ -14,14 +16,42 @@ const getRandomCard = (cards) => {
     return cardDrawn;
 };
 
+const getRandomSuit = (suits) => {
+    const randomNumber = Math.floor(Math.random() * suits.length);
+    const suitDrawn = suits[randomNumber];
+    return suitDrawn;
+}
+
+
+const displayCards = () => {
+    dealerSide.innerHTML += `<div class="card">
+    <h3 class="card__content card__content--top">${dealer[0]}</h3>
+    <h3 class="card__content card__content--middle">${getRandomSuit(suits)}</h3>
+    <h3 class="card__content card__content--bottom">${dealer[0]}</h3>
+    </div>`;
+
+    for (let i = 0; i < 2; i++) {
+        playerSide.innerHTML += `<div class="card">
+        <h3 class="card__content card__content--top">${player[i]}</h3>
+        <h3 class="card__content card__content--middle">${getRandomSuit(suits)}</h3>
+        <h3 class="card__content card__content--bottom">${player[i]}</h3>
+        </div>`;
+    }
+};
+
 const startGame = () => {
+    // get random cards for both dealer and player
     for (let i = 0; i < 2; i++) {
         dealer.push(getRandomCard(deckOfCard));
         player.push(getRandomCard(deckOfCard));
     }
+
+    // display their cards on hand
+    displayCards();
+
 };
 
-// Event listener
+// Event listeners
 startBtn.addEventListener("click", startGame);
 
 
