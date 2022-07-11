63835351326401ec5db89f8ae07d394975046225
Chris Ko <ko_ka_chun@hotmail.com>
2022-07-11 23:59:35 +0100

    created an array of cards and a function to get random card.

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 
+const deckOfCard = ["A", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+let dealer = [];
+let player = [];
+
+const getRandomCard = (cards) => {
+    const randomNumber = Math.floor(Math.random() * cards.length);
+    const cardDrawn = cards[randomNumber];
+    return cardDrawn;
+};
+
 
 
 
